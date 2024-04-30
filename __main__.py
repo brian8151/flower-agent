a6abaafd7ec964bbf39752dadc9c5b798bf24a96
brian8151 <xunwu8151@yahoo.com>
2024-04-30 17:39:37 -0400

    add logs

--- __main__.py
+++ __main__.py
@@ -1,33 +1,32 @@
 import argparse
 import os
 # from src.client.flwr_client import create_client
-from src.client.onyx_flwr_client import create_client
+from src.client.onyx_flwr_client import create_client, FlowerClient
 # from src.client.flwr_client import create_client as flwr_create_client
 import flwr as fl
 from src.util import log
 
 logger = log.init_logger()
-def parse_args():
-    parser = argparse.ArgumentParser(description="Run Flower client")
-    parser.add_argument(
-        "--node-id",
-        type=int,
-        choices=[0, 1, 2],
-        required=True,
-        help="Partition of the dataset (0, 1, or 2)."
-    )
-    return parser.parse_args()
+# def parse_args():
+#     parser = argparse.ArgumentParser(description="Run Flower client")
+#     parser.add_argument(
+#         "--node-id",
+#         type=int,
+#         choices=[0, 1, 2],
+#         required=True,
+#         help="Partition of the dataset (0, 1, or 2)."
+#     )
+#     return parser.parse_args()
 
 
 def main():
 
     # Make TensorFlow log less verbose
     os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-    args = parse_args()
+    # args = parse_args()
     try:
         logger.info("Onyx Federated Learning Agent starting ...")
-        client = create_client(args.node_id)
-        fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=client)
+        fl.client.start_client(server_address="127.0.0.1:8080", client=FlowerClient().to_client())
         logger.info("Onyx Flower client setup completed")
 
     except Exception as e:
