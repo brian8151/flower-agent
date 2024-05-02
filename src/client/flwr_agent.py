4a6afe820424a054cb5c19ccc8139b3c586121d6
brian8151 <xunwu8151@yahoo.com>
2024-05-02 15:39:37 -0400

    clean up

--- src/client/flwr_agent.py
+++ src/client/flwr_agent.py
@@ -34,26 +34,3 @@ def setup_and_load_data(partition_id, data_path, test_size=0.2, random_seed=42):
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     return model, x_train, y_train, x_test, y_test
-
-
-
-def setup_and_load_data_old(partition_id):
-    # Make TensorFlow log less verbose
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-    data ="payment.csv"
-    # Load model and data (MobileNetV2, CIFAR-10)
-    model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
-    model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
-
-    # Download and partition dataset
-    fds = FederatedDataset(dataset="cifar10", partitioners={"train": 3})
-    partition = fds.load_partition(partition_id, "train")
-    partition.set_format("numpy")
-
-    # Divide data on each node: 80% train, 20% test
-    partition = partition.train_test_split(test_size=0.2, seed=42)
-    x_train, y_train = partition["train"]["img"] / 255.0, partition["train"]["label"]
-    x_test, y_test = partition["test"]["img"] / 255.0, partition["test"]["label"]
-
-
-    return model, x_train, y_train, x_test, y_test
