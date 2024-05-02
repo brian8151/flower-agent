6b2ca266ac93b8aab77ff672f4576d65a78eae85
brian8151 <xunwu8151@yahoo.com>
2024-05-02 14:14:14 -0400

    test

--- __main__.py
+++ __main__.py
@@ -1,60 +1,72 @@
 import argparse
 import os
-# # from src.client.flwr_client import create_client
-# from src.client.onyx_flwr_client import create_client
-# from src.client.flwr_client import create_client as flwr_create_client
-# import flwr as fl
-# from flwr.client import start_client
-from src.util import log
 
-logger = log.init_logger()
-# def parse_args():
-#     parser = argparse.ArgumentParser(description="Run Flower client")
-#     parser.add_argument(
-#         "--node-id",
-#         type=int,
-#         choices=[0, 1, 2],
-#         required=True,
-#         help="Partition of the dataset (0, 1, or 2)."
-#     )
-#     return parser.parse_args()
+from flwr.client import ClientApp, NumPyClient
+import tensorflow as tf
+from flwr_datasets import FederatedDataset
 
-from client import client_fn, FlowerClient  # Import necessary functions from client.py
-from flwr.client import start_client
+# Make TensorFlow log less verbose
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-def main():
+# Parse arguments
+parser = argparse.ArgumentParser(description="Flower")
+parser.add_argument(
+    "--partition-id",
+    type=int,
+    choices=[0, 1, 2],
+    default=0,
+    help="Partition of the dataset (0, 1 or 2). "
+    "The dataset is divided into 3 partitions created artificially.",
+)
+args, _ = parser.parse_known_args()
 
-    # Make TensorFlow log less verbose
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-    logger.info("Onyx Federated Learning Agent starting ...")
-    # args = parse_args()
-    # try:
-    #
-    #     # Create and start the Flower client
-    #     logger.info(" Create and start the Flower client ...")
-    #     client = create_client()
-    #     logger.info("Onyx Federated Learning Agent starting ...")
-    #     start_client(
-    #         server_address="127.0.0.1:8080",
-    #         client=client.to_client()
-    #     )
-    #     logger.info("Onyx Flower client setup completed")
-    #
-    # except Exception as e:
-    #     logger.error("Failed to start the onyx flwr client: %s", str(e))
-    try:
-        app = ClientApp(
-            client_fn=client_fn,
-        )
-        app.start()
+# Load model and data (MobileNetV2, CIFAR-10)
+model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 
-        # If you're using legacy mode
-        start_client(
-            server_address="127.0.0.1:8080",
-            client=FlowerClient().to_client(),
-        )
-    except Exception as e:
-        logger.error("Failed to start the flwr client: %s", str(e))
+# Download and partition dataset
+fds = FederatedDataset(dataset="cifar10", partitioners={"train": 3})
+partition = fds.load_partition(args.partition_id, "train")
+partition.set_format("numpy")
 
+# Divide data on each node: 80% train, 20% test
+partition = partition.train_test_split(test_size=0.2, seed=42)
+x_train, y_train = partition["train"]["img"] / 255.0, partition["train"]["label"]
+x_test, y_test = partition["test"]["img"] / 255.0, partition["test"]["label"]
+
+
+# Define Flower client
+class FlowerClient(NumPyClient):
+    def get_parameters(self, config):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=1, batch_size=32)
+        return model.get_weights(), len(x_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        return loss, len(x_test), {"accuracy": accuracy}
+
+
+def client_fn(cid: str):
+    """Create and return an instance of Flower `Client`."""
+    return FlowerClient().to_client()
+
+
+# Flower ClientApp
+app = ClientApp(
+    client_fn=client_fn,
+)
+
+
+# Legacy mode
 if __name__ == "__main__":
-    main()
+    from flwr.client import start_client
+
+    start_client(
+        server_address="127.0.0.1:8080",
+        client=FlowerClient().to_client(),
+    )
