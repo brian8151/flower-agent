87ad4fe7296807e7a7eb67b3be32d2532c6bc642
brian8151 <xunwu8151@yahoo.com>
2024-05-02 16:11:15 -0400

    pass arg for file

--- __main__.py
+++ __main__.py
@@ -8,10 +8,9 @@ logger = log.init_logger()
 
 from flwr.client import ClientApp, NumPyClient
 from src.client.flwr_agent import setup_and_load_data
-
 def main():
-    # Parse arguments to get partition ID
-    parser = argparse.ArgumentParser(description="Flower")
+    # Parse arguments to get partition ID and CSV file name
+    parser = argparse.ArgumentParser(description="Flower Client Configuration")
     parser.add_argument(
         "--partition-id",
         type=int,
@@ -19,31 +18,36 @@ def main():
         default=0,
         help="Partition of the dataset (0, 1, or 2).",
     )
-    args, _ = parser.parse_known_args()
+    parser.add_argument(
+        "csv_file_name",
+        type=str,
+        help="Name of the CSV file to load data from."
+    )
+    args = parser.parse_args()
+
+    # Construct the file path
+    file_path = f'/apps/data/{args.csv_file_name}'
 
     # Setup TensorFlow and load data
-    model, x_train, y_train, x_test, y_test = setup_and_load_data(args.partition_id, '/apps/data/payment.csv')
+    model, x_train, y_train, x_test, y_test = setup_and_load_data(args.partition_id, file_path)
+
     # Create a Flower client instance
     flower_client = FlowerClient(model, x_train, y_train, x_test, y_test)
+
     def client_fn(cid: str):
         """Create and return an instance of Flower `Client`."""
         return flower_client.to_client()
 
     # Flower ClientApp
-    app = ClientApp(
-        client_fn=client_fn,
-    )
+    app = ClientApp(client_fn=client_fn)
 
     # Start the Flower client
     try:
         logger.info("Starting the Flower client...")
-        start_client(
-            server_address="127.0.0.1:8080",
-            client=flower_client.to_client(),
-        )
+        start_client(server_address="127.0.0.1:8080", client=flower_client.to_client())
         logger.info("Flower client setup completed")
     except Exception as e:
-        logger.error("Failed to start the Flower client: %s", str(e))
+        logger.error(f"Failed to start the Flower client: {str(e)}")
 
 if __name__ == "__main__":
     main()
