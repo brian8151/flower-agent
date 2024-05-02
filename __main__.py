b49671a6c8e8504188813db82c024c7746009b99
brian8151 <xunwu8151@yahoo.com>
2024-05-02 14:38:38 -0400

    update test

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 import argparse
 
 from flwr.client import start_client
+
+from client import FlowerClient
 from src.util import log
 logger = log.init_logger()
 
@@ -21,25 +23,11 @@ def main():
 
     # Setup TensorFlow and load data
     model, x_train, y_train, x_test, y_test = setup_and_load_data(args.partition_id)
-
-    # Define Flower client
-    class FlowerClient(NumPyClient):
-        def get_parameters(self, config):
-            return model.get_weights()
-
-        def fit(self, parameters, config):
-            model.set_weights(parameters)
-            model.fit(x_train, y_train, epochs=1, batch_size=32)
-            return model.get_weights(), len(x_train), {}
-
-        def evaluate(self, parameters, config):
-            model.set_weights(parameters)
-            loss, accuracy = model.evaluate(x_test, y_test)
-            return loss, len(x_test), {"accuracy": accuracy}
-
+    # Create a Flower client instance
+    flower_client = FlowerClient(model, x_train, y_train, x_test, y_test)
     def client_fn(cid: str):
         """Create and return an instance of Flower `Client`."""
-        return FlowerClient().to_client()
+        return flower_client.to_client()
 
     # Flower ClientApp
     app = ClientApp(
@@ -51,7 +39,7 @@ def main():
         logger.info("Starting the Flower client...")
         start_client(
             server_address="127.0.0.1:8080",
-            client=FlowerClient().to_client(),
+            client=flower_client.to_client(),
         )
         logger.info("Flower client setup completed")
     except Exception as e:
