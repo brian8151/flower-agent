af19ddc91d4cebd858ca7129f625a0a13fe9c46c
brian8151 <xunwu8151@yahoo.com>
2024-05-20 17:16:15 -0400

    update request

--- src/datamodel/data_item.py
+++ src/datamodel/data_item.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
 
 
 class DataItem(BaseModel):
-    features: List[float]
+    features: List[float] = Field(..., alias="features", description="prediction data features")
 
 
 class PredictionRequest(BaseModel):
-    domain_type: str
-    workflowtraceId: str
-    data: List[DataItem]
+    domain_type: str = Field(..., alias="domainType", description="data seed domain type")
+    workflow_trace_id: str = Field(..., alias="workflowTraceId", description="workflow trace id")
+    data: List[DataItem] = Field(..., alias="data", description="prediction data list")
