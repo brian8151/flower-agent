09944141767d44e3950209f24f6bb9a311ba8fe2
brian8151 <xunwu8151@yahoo.com>
2024-05-20 20:14:35 -0400

    add self

--- src/util/app_util.py
+++ src/util/app_util.py
@@ -1,6 +1,6 @@
 
 
-def convert_json_to_python(data):
+def convert_json_to_python(self, data):
     if isinstance(data, dict):
         for key, value in data.items():
             if value == "null":
