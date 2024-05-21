d1f9d09af4487197b63ded5b71a00ccba1799b16
brian8151 <xunwu8151@yahoo.com>
2024-05-21 13:29:27 -0400

    load conf

--- __main__.py
+++ __main__.py
@@ -3,9 +3,13 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from src.router.agent_flower_router import flower_router
 import logging
-
 from src.service.model_operator import ModelOperator
-
+import sys
+from os.path import dirname as opd, realpath as opr
+_BASEDIR_ = opd(opr(__file__))
+sys.path.append(_BASEDIR_)
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=_BASEDIR_+"/app.env")
 
 def create_app():
     logging.getLogger("uvicorn.error").setLevel(logging.INFO)
