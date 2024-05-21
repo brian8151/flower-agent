7e0506dfe905efc4fafaaa83bbcbe922f9a7290f
brian8151 <xunwu8151@yahoo.com>
2024-05-20 23:13:04 -0400

    test

--- src/mlmodel/model_builder.py
+++ src/mlmodel/model_builder.py
@@ -14,19 +14,19 @@ def build_model_from_config(config):
         layer_kwargs = layer_config['config']
 
         # Handle special cases such as input shape
-        if 'batch_input_shape' in layer_kwargs:
+        if layer_kwargs.get('batch_input_shape') is not None:
             layer_kwargs['batch_input_shape'] = tuple(
                 None if dim is None else dim for dim in layer_kwargs['batch_input_shape']
             )
 
-        if 'build_config' in layer_kwargs and 'input_shape' in layer_kwargs['build_config']:
+        if layer_kwargs.get('build_config') and 'input_shape' in layer_kwargs['build_config']:
             layer_kwargs['input_shape'] = tuple(
                 None if dim is None else dim for dim in layer_kwargs['build_config']['input_shape']
             )
 
         # Filter out keys that are not valid for this layer type
         valid_args = class_.__init__.__code__.co_varnames
-        layer_kwargs = {k: v for k, v in layer_kwargs.items() if k in valid_args}
+        layer_kwargs = {k: v for k, v in layer_kwargs.items() if k in valid_args and v is not None}
 
         # Create layer and add to model
         layer = class_(**layer_kwargs)
