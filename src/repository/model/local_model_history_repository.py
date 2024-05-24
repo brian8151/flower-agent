5ba061f75345f19a138693da62d2e9d4012e8994
brian8151 <xunwu8151@yahoo.com>
2024-05-24 18:46:24 -0400

    update max version

--- src/repository/model/local_model_history_repository.py
+++ src/repository/model/local_model_history_repository.py
@@ -10,6 +10,8 @@ def create_local_model_historical_records(workflow_trace_id, name, model_weights
         workflow_trace_id (str): workflow trace id.
         model_weights (str): local model weights.
     """
-    sql = """insert into agent_local_model_history (workflow_trace_id, name, model_weights) VALUES('{}', '{}', '{}')""".format(
-        workflow_trace_id, name, model_weights)
+    sql = """
+    INSERT INTO agent_local_model_history (workflow_trace_id, name, model_weights, version) 
+    VALUES ('{}', '{}', '{}', (SELECT IFNULL(MAX(version), 0) + 1 FROM agent_local_model_history WHERE name = '{}'))
+    """.format(workflow_trace_id, name, model_weights, name)
     DBConnection.execute_update(sql)
