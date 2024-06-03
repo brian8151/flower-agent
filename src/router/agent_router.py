564ae9c0d5ea8d7c1b2e069c7194105682405c7c
brian8151 <xunwu8151@yahoo.com>
2024-06-03 13:28:00 -0400

    add health test

--- src/router/agent_router.py
+++ src/router/agent_router.py
@@ -15,27 +15,20 @@ agent_router = APIRouter()
 async def initial_weights(request: WeightRequest):
     try:
         model_runner = ModelRunner()
-        weights = model_runner.initial_weights(request.name, request.domain,request.version, request.model)
+        weights = model_runner.initial_weights(request.name, request.domain, request.version, request.model)
         return {"status": "success", "domain": request.domain, "weights": weights}
     except Exception as e:
         logger.error(f"Error getting model weights: {e}")
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
 
 
-# @agent_router.post("/predict-weights")
-# async def predict(request: PredictionWithWeightReq):
-#     try:
-#         logger.info(f"Domain Type: {request.domain_type}")
-#         logger.info(f"Workflow Trace ID: {request.workflow_trace_id}")
-#         logger.info(f"data_received: {len(request.data)}")
-#         model_runner = ModelRunner()
-#         data_req = model_runner.run_model_prediction(request.workflow_trace_id, request.domain_type, request.data)
-#         for item in request.data:
-#             logger.info(f"Received data: {item.features}")
-#         return {"status": "success", "data_received": len(request.data), "predictions": data_req}
-#     except Exception as e:
-#         logger.error(f"Error predict data: {e}")
-#         raise HTTPException(status_code=500, detail="Internal Server Error")
+@agent_router.get("/health")
+async def health():
+    try:
+        return {"status": "success"}
+    except Exception as e:
+        logger.error(f"Error getting health: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
 
 
 @agent_router.post("/predict")
@@ -45,7 +38,7 @@ async def predict_data(request: PredictRequest):
         logger.info(f"Workflow Trace ID: {request.workflow_trace_id}")
         model_runner = ModelRunner()
         data_req = model_runner.run_model_predict(request.workflow_trace_id, request.domain_type, request.batch_id)
-        return {"status": "success",  "predictions": data_req}
+        return {"status": "success", "predictions": data_req}
     except Exception as e:
         logger.error(f"Error predict data: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
