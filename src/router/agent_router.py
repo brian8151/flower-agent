c35b6272f6e8ec807245adcf5ab4e2c794647a8d
brian8151 <xunwu8151@yahoo.com>
2024-06-09 23:13:27 -0400

    update agent

--- src/router/agent_router.py
+++ src/router/agent_router.py
@@ -11,6 +11,16 @@ logger = log.init_logger()
 agent_router = APIRouter()
 
 
+@agent_router.get("/health")
+async def check_health():
+    try:
+        # You can add any specific health check logic here if needed
+        return {"message": "Service is healthy", "success": True}
+    except Exception as e:
+        logger.error("Health check failed: {0}".format(str(e)))
+        return {"message": "Service is not healthy", "success": False}
+
+
 @agent_router.post("/initial-weights")
 async def initial_weights(request: WeightRequest):
     try:
@@ -48,7 +58,8 @@ async def predict_data(request: PredictRequest):
         logger.info(f"Domain Type: {request.domain_type}")
         logger.info(f"Workflow Trace ID: {request.workflow_trace_id}")
         model_runner = ModelRunner()
-        status, workflow_trace_id, n = model_runner.run_model_predict(request.workflow_trace_id, request.domain_type, request.batch_id)
+        status, workflow_trace_id, n = model_runner.run_model_predict(request.workflow_trace_id, request.domain_type,
+                                                                      request.batch_id)
         return {"status": status, "workflow_trace_id": workflow_trace_id, "items": n}
     except Exception as e:
         logger.error(f"Error predict data: {e}")
@@ -61,7 +72,8 @@ async def training_data(request: TraningRequest):
         logger.info(f"Domain Type: {request.domain_type}")
         logger.info(f"Workflow Trace ID: {request.workflow_trace_id}")
         model_runner = ModelRunner()
-        status, workflow_trace_id, loss, num_examples, metrics = model_runner.run_model_training(request.workflow_trace_id, request.domain_type, request.batch_id)
+        status, workflow_trace_id, loss, num_examples, metrics = model_runner.run_model_training(
+            request.workflow_trace_id, request.domain_type, request.batch_id)
         return {"status": status, "workflow_trace_id": workflow_trace_id, "num_examples": num_examples}
     except Exception as e:
         logger.error(f"Error predict data: {e}")
