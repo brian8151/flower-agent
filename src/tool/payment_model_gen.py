3905315c9943c3e67b656f7d27b43f24390601c1
brian8151 <xunwu8151@yahoo.com>
2024-06-10 17:58:26 -0400

    update model

--- src/tool/payment_model_gen.py
+++ src/tool/payment_model_gen.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 
 
-def build_model(self):
+def build_model():
     model = tf.keras.Sequential([
-        tf.keras.layers.InputLayer(input_shape=(27)),
+        tf.keras.layers.InputLayer(input_shape=(27,)),  # Fixed input_shape to be a tuple
         tf.keras.layers.Dense(32, activation='tanh'),
         tf.keras.layers.Dense(64, activation='tanh'),
         tf.keras.layers.Dense(2, activation="softmax")
@@ -15,6 +15,11 @@ def build_model(self):
     )
     return model
 
-model_json = build_model.to_json()
+
+# Build the model
+model = build_model()
+
+# Convert the model to JSON
+model_json = model.to_json()
 
 print(model_json)
